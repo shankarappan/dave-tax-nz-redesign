3b12efd566d05067020702db8bdef0b1852c52e3
Shankar Ramanathan <shankar@Shankars-MacBook-Pro-M5.local>
2026-08-20 09:50:36 +1200

    Configure build for custom domain

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/dave-tax-nz-redesign/" : "/",
+  base: "/",
   build: {
     outDir: "dist/client",
   },
